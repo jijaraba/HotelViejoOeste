6c41735cf63c7b40516d62fcf8fdc918969ce991
José Ignacio Jaraba Atehortúa <jijaraba@hotmail.com>
2015-06-23 21:46:35 -0500

    Import Cabins

--- app/database/seeds/CabinsTableSeeder.php
+++ app/database/seeds/CabinsTableSeeder.php
@@ -1,6 +1,5 @@
 <?php
 
-use Faker\Factory as Faker;
 
 class CabinsTableSeeder extends Seeder
 {
@@ -12,19 +11,6 @@ class CabinsTableSeeder extends Seeder
     public function run()
     {
         DB::table('cabins')->delete();
-        $faker = Faker::create();
-        foreach (range(1, 9) as $index) {
-
-            $name =  $faker->citySuffix;
-
-            Cabin::create([
-                'name' => ucfirst($name),
-                'description' => $faker->sentence($nbWords = 40),
-                'capacity' => $faker->randomDigitNotNull,
-                'rate_ls' => $faker->numberBetween($min = 200000, $max = 1200000),
-                'rate_hs' => $faker->numberBetween($min = 200000, $max = 1200000),
-                'slug' => $name
-            ]);
-        }
+        DB::unprepared(file_get_contents('dummies/cabins.sql'));
     }
 }
