78721d92cf2e9ec5b733f7b2b1b1ed9645874e81
José Ignacio Jaraba Atehortúa <jijaraba@hotmail.com>
2015-07-01 19:50:13 -0500

    Cambio de Foto Restautante

--- app/views/pages/index.blade.php
+++ app/views/pages/index.blade.php
@@ -21,12 +21,12 @@
     <noscript><img height="1" width="1" alt="" style="display:none" src="https://www.facebook.com/tr?ev=6024279725873&amp;cd[value]=1.00&amp;cd[currency]=COP&amp;noscript=1" /></noscript>
 @stop
 @section("content")
-    <div id="modal" style="z-index:999;display:block;width:600px;height:300px;position:fixed;background:rgba(255,255,255,0.9);border-radius:10px;">
+    <div id="modal1" style="z-index:999;display:block;width:600px;height:300px;position:fixed;background:rgba(255,255,255,0.9);border-radius:10px;">
         <img style="width:600px;height: 300px" src="/img/splash.jpg" alt="Marcamos la diferencia reinventándonos para ti"/>
         <h2 style="position: absolute;width:100%;top:70px;text-align: center;font-family: 'Museo Sans 300';font-size: 2.2em;color:#ca9c2e;animation: 1s linear 0s normal none infinite running scale_title;">Marcamos la diferencia</h2>
         <h4 style="position: absolute;width:100%;top:120px;text-align: center;font-family: 'Museo Sans 100';font-size: 1.5em">Reinventándonos para ti</h4>
     </div>
-    <div id="mask" style="position:absolute;z-index:998;background-color:rgba(0,0,0,0.5);display:none;"></div>
+    <div id="mask1" style="position:absolute;z-index:998;background-color:rgba(0,0,0,0.5);display:none;"></div>
     <script type="text/javascript">
         //Get the screen height and width
         var maskHeight = $(document).height();
