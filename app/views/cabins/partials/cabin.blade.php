111ff688d362b1aa16c64ed3ffd6075fc2264eb4
José Ignacio Jaraba Atehortúa <jijaraba@hotmail.com>
2015-11-12 19:01:16 -0500

    Eliminación de Minibar en Cabañas

--- app/views/cabins/partials/cabin.blade.php
+++ app/views/cabins/partials/cabin.blade.php
@@ -53,8 +53,7 @@
             <li class="col-45 gutter-5 ls"><span>Temporada baja</span><br>${{ $cabin->rate_ls }}</li>
             <li class="col-10 gutter-5"></li>
             <li class="col-45 gutter-5 hs"><span>Temporada alta</span><br>${{ $cabin->rate_hs }}</li>
-            <li class="col-100">Incluye desayuno al estilo <strong>Viejo Oeste</strong> y consumo del
-                <strong>minibar</strong> de la cabaña
+            <li class="col-100">Incluye desayuno al estilo <strong>Viejo Oeste</strong>
             </li>
         </ul>
     </div>
