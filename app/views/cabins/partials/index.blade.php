3312f691de8dc7d2b63348154a397a56335e79d7
José Jaraba <jijaraba@hotmail.com>
2016-02-24 21:01:56 -0500

    Campaña Semana Santa

--- app/views/cabins/partials/index.blade.php
+++ app/views/cabins/partials/index.blade.php
@@ -1,4 +1,7 @@
 <article class="cabin-detail">
     <h1 class="title">Hotel Viejo Oeste</h1>
     <section class="description">Selecciona una de nuestras cabañas para empezar una experiencia diferente</section>
+    <div class="cabin-campaign">
+        <img src="img/campaign/campaign.png" alt="Promoción Semana Santa">
+    </div>
 </article>
